3f3d9f1bc0d1fb6a01318daf792ea716c93cbc6d
Michael Fehr <javacrypto@gmx.de>
2020-03-18 23:19:48 +0100

    Add files via upload

---
+++ BitcoinJ Client LunaTKWallet/src/mywallet/App.java
@@ -0,0 +1,42 @@
+package mywallet;
+
+/*
+ * Herkunft/Origin: http://javacrypto.bplaced.net/
+ * Programmierer/Programmer: Michael Fehr
+ * Copyright/Copyright: Michael Fehr
+ * Lizenttext/Licence: verschiedene Lizenzen / several licenses
+ * e.g. Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * getestet mit/tested with: Java Runtime Environment 11.0.5 x64
+ * verwendete IDE/used IDE: intelliJ IDEA 2019.3.1
+ * Datum/Date (dd.mm.jjjj): 18.03.2020
+ * Programm/Program: BitcoinJ Client LunaTKWallet
+ * Funktion: zeigt die Erstellung eines funtionierenden Bitcoin Clients
+ * Function: show the creating of a working bitcoin client
+ *
+ * Sicherheitshinweis/Security notice
+ * Die Programmroutinen dienen nur der Darstellung und haben keinen Anspruch auf eine korrekte Funktion,
+ * insbesondere mit Blick auf die Sicherheit !
+ * Prüfen Sie die Sicherheit bevor das Programm in der echten Welt eingesetzt wird.
+ * The program routines just show the function but please be aware of the security part -
+ * check yourself before using in the real world !
+ *
+ * Sie benötigen diverse Bibliotheken (alle im Github-Archiv im Unterordner "libs")
+ * You need a lot of libraries (see my Github-repository in subfolder "libs")
+ * verwendete BitcoinJ-Bibliothek / used BitcoinJ Library: bitcoinj-core-0.15.6.jar
+ * my Github-Repository: https://github.com/java-crypto/BitcoinJ
+ * libs in my Github-Repo: https://github.com/java-crypto/BitcoinJ_Libraries
+ *
+ * Source/Basis: https://github.com/LunaTK/Simple-Bitcoinj-GUI-Wallet
+ */
+
+import javafx.application.Platform;
+import org.bitcoinj.utils.Threading;
+
+public class App {
+    public static void main(String args[]) {
+        Threading.USER_THREAD = (Runnable runnable) -> {
+            Platform.runLater(runnable);
+        };
+        MyBitcoinWallet.main(args);
+    }
+}
